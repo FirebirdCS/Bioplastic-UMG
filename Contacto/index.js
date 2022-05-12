6c3ae2d8e480fcc2377ce2ad6e068bb891493333
alvaro florez flores perez <alvaritotutos@gmail.com>
2022-05-12 16:01:12 -0600

    Inputs fixed

--- Contacto/index.js
+++ Contacto/index.js
@@ -1,4 +1,8 @@
 function ClearFields()
 {
     document.getElementById("firstName").value = "";
+    document.getElementById("secondName").value = "";
+    document.getElementById("phoneNumber").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("floatingTextarea").value = "";
 }
